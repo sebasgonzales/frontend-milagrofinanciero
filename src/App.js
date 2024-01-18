6880deaa0f3d5eef6e4c4b5038a7b3dc8393b619
Sebastian Gonzales <sebastian.gt.2002@gmai.com>
2024-01-18 11:45:37 -0300

    navbar terminado

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/navegacion/navbar";
 import Home from './screens/Home';
-import Transferencia from './screens/Transferencia';
+import Transferencia from './screens/transferencia/Transferencia';
 import MiCuenta from "./screens/MiCuenta";
 import Configuracion from "./screens/Configuracion";
 
--- src/components/navegacion/navbar.js
+++ src/components/navegacion/navbar.js
@@ -1,40 +1,42 @@
 import React from 'react'
-import './Navbar.module.css'
+import { Link } from 'react-router-dom'
 
 const navbar = () => {
   return (
     <div>
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
-    <a class="navbar-brand" href="#">Milagro Financiero</a>
+    <a class="navbar-brand" href=".">Milagro Financiero</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
+    <div class="d-flex justify-content-end">
     <div className="collapse navbar-collapse" id="navbarNavDropdown">
       <ul className="navbar-nav me-auto">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+          <Link className="nav-link active " aria-current="page" to='../screens/Home' style={{marginRight: 50 + 'px'}}>Home</Link>
         </li>
         <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <Link className=" nav-item nav-link dropdown-toggle" href="." role="button" data-bs-toggle="dropdown" aria-expanded="false" style={{marginRight: 50 + 'px'}} to='../screens/transferencia/Transferencia'>
             Transferencia
-          </a>
+          </Link>
           <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="#">Nueva Transferencia</a></li>
-            <li><a className="dropdown-item" href="#">Solicitar Pago</a></li>
-            <li><a className="dropdown-item" href="#">Cuentas Agendadas</a></li>
+            <li><a className="dropdown-item" href=".">Nueva Transferencia</a></li>
+            <li><a className="dropdown-item" href=".">Solicitar Pago</a></li>
+            <li><a className="dropdown-item" href=".">Cuentas Agendadas</a></li>
           </ul>
         </li>
         
-        <li className="nav-item">
-          <a className="nav-link" href="#">Mi Cuenta</a>
+        <li className="nav-item" style={{marginRight: 50 + 'px'}}>
+          <Link className="nav-link" to='../screens/MiCuenta'>Mi Cuenta</Link>
         </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Configuracion</a>
+        <li className="nav-item" style={{marginRight: 50 + 'px'}}>
+          <Link className="nav-link" to='../screens/Configuracion'>Configuracion</Link>
         </li>
       </ul>
       
     </div>
+  </div>
   </div>
 </nav>
     </div>
