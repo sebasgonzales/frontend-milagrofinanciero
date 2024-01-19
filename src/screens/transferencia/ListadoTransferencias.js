d96d518e8be4b5d3016b7c57201e0be97dc99e83
Nuria Robledo <nuriarobledo0@gmail.com>
2024-01-19 10:33:21 -0300

    transf en cero

---
+++ src/screens/transferencia/ListadoTransferencias.js
@@ -0,0 +1,14 @@
+import {useEffect} from 'react';
+
+export default function TaskList() {
+
+       
+        return (
+            <>
+            <h1>Historial de transferencias</h1>
+            
+            </>
+        )
+        
+       
+}
