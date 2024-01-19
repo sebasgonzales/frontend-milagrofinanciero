1a7dcabcc7db4077013eb7e4264e827031c9e912
Sebastian Gonzales <sebastian.gt.2002@gmai.com>
2024-01-19 14:27:10 -0300

    Se hizo una tabla con todas las transacciones

--- src/screens/transferencia/Transferencia.js
+++ src/screens/transferencia/Transferencia.js
@@ -1,12 +1,18 @@
 import React from 'react'
-
+import ListadoTransferencias from './ListadoTransferencias'
 const Transferencia = () => {
   return (
-    <div className='container text-left'>
-      <div className='row align-items-center'>
-        <div className='col-8'>
-          <p class="fs-3">Cuenta N° 123456789</p>
+    <div>
+      <div className='container text-left'>
+        <div className='row align-items-center'>
+          <div className='col-8'>
+            <p class="fs-3">Cuenta N° 123456789</p>
+
+          </div>
         </div>
+      </div>
+      <div className='container text-center'>
+        <ListadoTransferencias></ListadoTransferencias>
       </div>
     </div>
   )
