227e88cc4929a86c19b3727b7dfe5edc954f1847
Nuria Robledo <nuriarobledo0@gmail.com>
2024-01-20 16:40:07 -0300

    boton de nuevaTrans y Solicitar pago

--- src/screens/transferencia/Transferencia.js
+++ src/screens/transferencia/Transferencia.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ListadoTransferencias from '../../components/ListadoTransferencias'
 import NuevaTransferencia from './NuevaTransferencia'
+import { Link } from 'react-router-dom'
+import Button from 'react-bootstrap/Button';
 
 const Transferencia = () => {
   return (
@@ -12,10 +14,24 @@ const Transferencia = () => {
         <div className='row align-items-center'>
           <div className='col-8'>
             <p class="fs-3">Cuenta N° 123456789</p>
-
           </div>
+          <div className="text-center">
+          <div className="mb-4 justify-content-center">
+          <Link to="/screens/transferencia/NuevaTransferencia"> 
+        <Button variant="primary " size="lg" style={{marginRight: 5 + 'px'}}>
+          Nueva Transferencia
+        </Button>
+        </Link>{' '}
+        <Link to="/screens/transferencia/SolicitarPago"> 
+        <Button variant="primary" size="lg" style={{marginLeft: 5 + 'px'}}>
+          Solicitar Pago
+        </Button>
+        </Link>
+      </div>
+      </div>
         </div>
       </div>
+      
       <div className='container text-center'>
         <ListadoTransferencias></ListadoTransferencias>
       </div>
