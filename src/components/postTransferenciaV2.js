2d683dc27e72485d962ecf5cdda3f9ed0d3a42cd
Nuria Robledo <nuriarobledo0@gmail.com>
2024-01-22 22:24:32 -0300

    hay que rellenar los campos requeridos obligatoriamente para hacer la transaccion, sino no se hace (antes se hacia igual)

--- src/components/postTransferenciaV2.js
+++ src/components/postTransferenciaV2.js
@@ -5,10 +5,13 @@ import { Form, Button, Alert } from 'react-bootstrap';
 import Col from 'react-bootstrap/Col';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Row from 'react-bootstrap/Row';
+import { toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 //import { Form, Button } from 'react-bootstrap';
 import './postTransferencia.css';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+
 import Container from 'react-bootstrap/Container';
 
 function PostTransferenciaV2() {
@@ -143,15 +146,25 @@ function PostTransferenciaV2() {
 
 
   //FUNCION PARA HACER LA TRANSFERENCIA----------
-  const realizarTransaccion = async () => {
+  const realizarTransaccion = async (event) => {
     try {
+      // Evita que el formulario se envíe y la página se recargue
+      event.preventDefault();
 
+      // Verifica si CbuDestino, monto y referencia tienen valores
+      if (!CbuDestino || !monto || !referencia) {
+        // Muestra un mensaje de error utilizando react-toastify o cualquier otra forma de manejar errores
+        toast.error('Por favor, complete todos los campos antes de realizar la transferencia');
+        console.log('Por favor, complete todos los campos antes de realizar la transferencia');
+        return;
+      }
+      
       const dataTransaccion = {
         Id: 0,
         Monto: monto,
         Acreditacion: fechaFormateada,
         Realizacion: fechaFormateada,
-        Motivo: "a ver confecha",
+        Motivo: "Intentando Nuria desde Front",
         Referencia: referencia,
         IdCuentaOrigen: 4,
         IdCuentaDestino: cuentaId,
@@ -165,124 +178,128 @@ function PostTransferenciaV2() {
       const response = await axios.post(`https://localhost:7042/Transaccion?numeroCuentaOrigen=6655443322&cbuDestino=${CbuDestino}&monto=${monto}`, dataTransaccion);
 
       console.log('Respuesta de la transacción:', response.data);
-      // Después de realizar la transferencia con éxito, muestra una notificación
-      await toast.success('Transferencia realizada con éxito');
 
-      // Espera unos segundos antes de recargar la página
-      setTimeout(() => {
-        window.location.reload();  // Recarga la página
-      }, 5000);  // 3000 milisegundos = 3 segundos (ajusta según tus necesidades)
+      // Muestra un mensaje de éxito utilizando react-toastify
+      toast.success('Transferencia realizada con éxito')
+    } catch (error) {
+      console.error('Error al realizar la transacción:', error.message);
+      console.error('Error al realizar la transacción:', error.message);
+      // Muestra un mensaje de error utilizando react-toastify
+      toast.error('Error al realizar la transferencia');
+    }
 
-    
-  } catch (error) {
-    console.error('Error al realizar la transacción:', error.message);
-  }
+  };
+  //----//
 
-};
-//----//
+  //--VALIDACION--//
+  const [validated, setValidated] = useState(false);
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
 
-//--VALIDACION--//
-const [validated, setValidated] = useState(false);
-const handleSubmit = (event) => {
-  const form = event.currentTarget;
-  if (form.checkValidity() === false) {
-    event.preventDefault();
-    event.stopPropagation();
-  }
+    setValidated(true);
+    if (form.checkValidity() === true) {
+      realizarTransaccion();
+    }
+  };
+  //----//
 
-  setValidated(true);
-};
-//----//
+  return (
+    <div>
+      <Form className="labelPersonalizado" noValidate validated={validated} onSubmit={handleSubmit}>
+        {/* <Form className="labelPersonalizado">  */}
+        <Form.Group as={Col} controlId="validationCustom01" className="align-items-start" >
+          <Form.Label>Cuenta Destino</Form.Label>
+          <InputGroup className="mb-3">
+            <Form.Control
+              type="text"
+              placeholder="Ingrese el cbu"
+              name="Cbu"
+              value={CbuDestino}
+              onChange={handleChangeCbuDestino}
+              aria-describedby="basic-addon2"
+              required
+            />
+            <Button variant="outline-secondary" id="button-addon2">
+              Buscar
+            </Button>
+          </InputGroup>
+          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+          <div>
+            {cuentaId !== null && (
+              <Alert variant="success">
+                ID de la cuenta destino: {cuentaId}
+              </Alert>
+            )}
 
-return (
+            {error && (
+              <Alert variant="danger">
+                Error al obtener la cuenta destino: {error}
+              </Alert>
+            )}
+          </div>
+        </Form.Group>
 
-  <Form className="labelPersonalizado" noValidate validated={validated} onSubmit={handleSubmit}>
-    {/* <Form className="labelPersonalizado">  */}
-    <Form.Group as={Col} controlId="validationCustom01" className="align-items-start" >
-      <Form.Label>Cuenta Destino</Form.Label>
-      <InputGroup className="mb-3">
-        <Form.Control
-          type="text"
-          placeholder="Ingrese el cbu"
-          name="Cbu"
-          value={CbuDestino}
-          onChange={handleChangeCbuDestino}
-          aria-describedby="basic-addon2"
-        />
-        <Button variant="outline-secondary" id="button-addon2">
-          Buscar
-        </Button>
-      </InputGroup>
-      <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-      <div>
-        {cuentaId !== null && (
-          <Alert variant="success">
-            ID de la cuenta destino: {cuentaId}
-          </Alert>
-        )}
+        <Form.Group as={Col} controlId="validationCustom02">
+          <Form.Label>Monto</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Ingrese el monto"
+            name="monto"
+            value={monto}
+            onChange={handleChangeMonto}
+            required
+          />
+          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group as={Col} controlId="validationCustomUsername">
+          <Form.Label>Motivo</Form.Label>
+          <select class="form-select" aria-label="Default select example">
+            <option selected>Seleccionar</option>
+            <option value="1">Varios</option>
+            <option value="2">Alquiler</option>
+            <option value="3">Three</option>
+          </select>
+        </Form.Group>
+        <Form.Group as={Col} controlId="validationCustom01">
+          <Form.Label>Referencia</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Ingrese la referencia"
+            name="referencia"
+            value={referencia}
+            onChange={handleChangeReferencia}
+            required
+          />
+          <Form.Control.Feedback type="invalid">
+            Por favor, ingrese una referencia.
+          </Form.Control.Feedback>
 
-        {error && (
-          <Alert variant="danger">
-            Error al obtener la cuenta destino: {error}
-          </Alert>
-        )}
-      </div>
-    </Form.Group>
-
-    <Form.Group as={Col} controlId="validationCustom02">
-      <Form.Label>Monto</Form.Label>
-      <Form.Control
-        type="text"
-        placeholder="Ingrese el monto"
-        name="monto"
-        value={monto}
-        onChange={handleChangeMonto}
-        required
-      />
-      <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-    </Form.Group>
-    <Form.Group as={Col} controlId="validationCustomUsername">
-      <Form.Label>Motivo</Form.Label>
-      <select class="form-select" aria-label="Default select example">
-        <option selected>Seleccionar</option>
-        <option value="1">Varios</option>
-        <option value="2">Alquiler</option>
-        <option value="3">Three</option>
-      </select>
-    </Form.Group>
-    <Form.Group as={Col} controlId="validationCustom01">
-      <Form.Label>Referencia</Form.Label>
-      <Form.Control
-        type="text"
-        placeholder="Ingrese la referencia"
-        name="referencia"
-        value={referencia}
-        onChange={handleChangeReferencia}
-
-        required
-      />
-    </Form.Group>
+        </Form.Group>
 
 
 
-    <div className="botonesAlPie mb-2">
-      <Button className="Btn2" variant="secondary" size="lg">
-        Cancelar
-      </Button>{' '}
-      <Button className="Btn1" type="submit" variant="primary" size="lg" onClick={realizarTransaccion}
-      >Transferir
-      </Button>
-       {/* Componente de Toast para mostrar notificaciones */}
-  <ToastContainer />
+        <div className="botonesAlPie mb-2">
+          <Button className="Btn2" variant="secondary" size="lg">
+            Cancelar
+          </Button>{' '}
+          <Button className="Btn1" type="submit" variant="primary" size="lg" onClick={realizarTransaccion}
+
+          >Transferir
+          </Button>
+
+        </div>
+
+
+      </Form>
+
+
+
     </div>
-
-
-  </Form>
-
-
-
-
-);
+  );
 }
 
 export default PostTransferenciaV2;
