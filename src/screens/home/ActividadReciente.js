263d5d5275a375d5cdaee6e3f96667a7cd2d403b
MilagrosSoberon <msoberon@alu.frlp.utn.edu.ar>
2024-01-25 18:53:28 -0300

    agregueActvReciente

---
+++ src/screens/home/ActividadReciente.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ActividadReciente = () => {
+  return (
+    <div>
+        <h1>Esta es la pagina de Actividad reciente</h1>
+    </div>
+  )
+}
+
+export default ActividadReciente
