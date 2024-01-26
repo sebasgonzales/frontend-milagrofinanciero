d79be5b19aacb58f9f781e4e183eb41681fa65e4
MilagrosSoberon <msoberon@alu.frlp.utn.edu.ar>
2024-01-26 11:41:17 -0300

    appPrincipal

--- src/App.js
+++ src/App.js
@@ -1,34 +1,12 @@
-import React from "react";
 import './App.css';
-import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import Navbar from "./components/navegacion/navbar";
+import AppPrincipal from './AppPrincipal';
 
-import Home from './screens/Home';
-import Transferencia from './screens/transferencia/Transferencia';
-import NuevaTransferencia from './screens/transferencia/NuevaTransferencia';
-import SolicitarPago from './screens/transferencia/SolicitarPago';
-import CuentasAgendadas from './screens/transferencia/CuentasAgendadas';
-import MiCuenta from "./screens/MiCuenta";
-import Configuracion from "./screens/Configuracion";
-
-
-function App(){
-    return (
-            <Router>
-                <Navbar>  
-                </Navbar>
-                <Routes>
-                    <Route path="/screens/Home" element={<Home />} />
-                    <Route path="/screens/transferencia/Transferencia" element={<Transferencia />}/>
-                    <Route path="/screens/transferencia/NuevaTransferencia" element={<NuevaTransferencia />}/>
-                    <Route path="/screens/transferencia/SolicitarPago" element={<SolicitarPago/>} />
-                    <Route path="/screens/transferencia/CuentasAgendadas" element={<CuentasAgendadas/>} />
-                    <Route path="/screens/MiCuenta" element={<MiCuenta />} />
-                    <Route path="/screens/Configuracion" element={<Configuracion/>}/>
-                </Routes>
-               {/* <NuevaTransferencia></NuevaTransferencia> */}
-            </Router>
-    )
+function App() {
+  return (
+    <div className="App">
+      <AppPrincipal></AppPrincipal>
+    </div>
+  );
 }
 
 export default App;
