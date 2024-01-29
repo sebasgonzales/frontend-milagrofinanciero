e0d3089b75e5ae41f0d3c2784109e348d74db5e7
Sebastian Gonzales <sebastian.gt.2002@gmai.com>
2024-01-29 02:36:30 -0300

    cambio de background color

--- src/components/Banner2.js
+++ src/components/Banner2.js
@@ -5,7 +5,7 @@ import { FaHouseLaptop } from "react-icons/fa6";
 
 const Banner = () => {
     return (
-        <div className='bg-secondary p-5 d-flex justify-content-center align-items-center' style={{ width: '100%' }}>
+        <div className='p-5 d-flex justify-content-center align-items-center' style={{ width: '100%', backgroundColor:'#ebebeb'}}>
             <div className="bg-white p-0 rounded-4 text-secondary shadow w-100 w-md-65">
                 <div className='container col-padding-5'>
                     <div className='row'>
@@ -20,7 +20,7 @@ const Banner = () => {
                             </p>
                             <p>¿Estás listo para tomar el control de tus finanzas?</p>
                         </div>
-                        <div className='col-sm-12 col-md-12 col-lg-3 pb-5 bg-light'>
+                        <div className='col-sm-12 col-md-12 col-lg-3 mb-3 pl-10 pt-3 bg-light'>
                             <div className='row text-right bg-light'>
                                 <h1><FaHouseLaptop /></h1>
                                 <h5>Tu banco,<br></br> desde tu hogar.</h5>
