4891f3b7447a5494b46445b4c8b664f05c896d17
Nacho Frangolini <131629947+Nachoff13@users.noreply.github.com>
2024-02-05 13:17:56 -0300

    el boton cambiar cuenta ya funciona perfectamente

--- src/components/cuenta/Saldo.js
+++ src/components/cuenta/Saldo.js
@@ -1,40 +1,40 @@
+// Saldo.js
+
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const Saldo = ({ cuentaSeleccionada }) => {
+const Saldo = ({ nroCuenta }) => {
   const [saldo, setSaldo] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  const obtenerSaldoPorCuenta = async (nroCuenta) => {
-      try {
-          const response = await axios.get(`https://localhost:7042/Transaccion/saldo/${nroCuenta}`);
-          setSaldo(response.data.saldoTotal);
-      } catch (error) {
-          setError("Error al obtener el saldo");
-      } finally {
-          setIsLoading(false);
-      }
+  const getSaldo = async (nroCuenta) => {
+    try {
+      const response = await axios.get(`https://localhost:7042/Transaccion/saldo/${nroCuenta}`);
+      setSaldo(response.data.saldoTotal);
+    } catch (error) {
+      setError("Error al obtener el saldo");
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   useEffect(() => {
-      if (cuentaSeleccionada) {
-          obtenerSaldoPorCuenta(cuentaSeleccionada);
-      }
-  }, [cuentaSeleccionada]);
+    getSaldo(nroCuenta);
+  }, [nroCuenta]);
 
   if (isLoading) {
-      return <p>Cargando saldo...</p>;
+    return <p>Cargando saldo...</p>;
   }
 
   if (error) {
-      return <p>{error}</p>;
+    return <p>{error}</p>;
   }
 
   return (
-      <div className='sal'>
-          <p>$ {saldo}</p>
-      </div>
+    <div className='sal'>
+      <p>$ {saldo}</p>
+    </div>
   );
 };
 
