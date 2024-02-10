afb379fca02a9a410f44481bda5d43fca1bf183b
Gonzalo Baez <gezbaez@gmail.com>
2024-02-10 18:51:53 -0300

    anda el usuario en home

--- src/components/cuenta/Usuario.js
+++ src/components/cuenta/Usuario.js
@@ -1,15 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Cookies from 'universal-cookie';
+
+const cookie = new Cookies();
+
+const cuitCuil = cookie.get('cuitCuil');
 
 const Usuario = ({ cuentaSeleccionada }) => {
   const [usuario, setUsuario] = useState('');
   
-  const getUsuario = async (username) => {
+  const getUsuario = async (cuitCuil) => {
     try {
       // Puedes ajustar la lógica según cómo se obtiene la información del usuario
-      const response = await axios.get(`https://localhost:7042/Cliente/clientes/Nombre/${username}/Cliente`);
-      console.log(response.data.nombre)
-      setUsuario(response.data.nombre);
+      const response = await axios.get(`https://localhost:7042/Cliente/clientes/Nombre/${cuitCuil}/Cliente`);
+      console.log(response.data)
+      setUsuario(response.data);
     } catch (error) {
       console.log("Error al obtener información del usuario", error);
     }
@@ -21,13 +26,13 @@ const Usuario = ({ cuentaSeleccionada }) => {
   //   }
   // }, [cuentaSeleccionada]); // Asegúrate de invocar la función cuando la cuenta seleccionada cambie
 
-  useEffect(() => {     getUsuario('deamon16');   }, []);
+   useEffect(() => {     getUsuario(cuitCuil);   }, []);
 
   return (
     // NO DEUVLEVE EL USUARIO PQ SE HACE EL LOGIN
     // REVISAR CUANDO ESTE EL LOGIN
     <div className="usuario">
-      <h1>Bienvenido {usuario}</h1>
+      <h1>Bienvenido/a {usuario}</h1>
     </div>
   );
 }
